8ac18aa904a6b98a37962d9c45091f3f2b2b705c
MuhammadN0 <117872290+MuhammadN0@users.noreply.github.com>
2023-02-27 01:58:48 +0200

    added enter feature

--- main.js
+++ main.js
@@ -48,3 +48,13 @@ myElement.onclick = function(){
   add.value =``
   remove.value =``
 }
+
+document.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    myElement.click();
+  }
+});
